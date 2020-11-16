c453505343f25af0a9dcb6ec9547b2dbf57c8e30
Raspberry Pi <raspberry@example.com>
2020-11-16 22:51:09 +0300

    rework initial index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+console.log('App started')
+app.get('/test', function (req, res) {
+    console.log('get testGet')
+	res.send('ok')
+   
+})
+ 
+
+app.listen(3000)
